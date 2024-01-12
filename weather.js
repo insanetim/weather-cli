65832eec8263f9fb5b92c0e01d781ca8e0cffd83
insanetim <47120724+insanetim@users.noreply.github.com>
2024-01-12 14:15:46 +0200

    added log service

---
+++ weather.js
@@ -0,0 +1,19 @@
+#!/usr/bin/env node
+import { getArgs } from './helpers/args.js'
+import { printHelp } from './services/log.service.js'
+
+const initCLI = () => {
+  const args = getArgs(process.argv)
+  if (args.h) {
+    printHelp()
+  }
+  if (args.s) {
+    // Save city
+  }
+  if (args.t) {
+    // Save token
+  }
+  // Output weather
+}
+
+initCLI()
